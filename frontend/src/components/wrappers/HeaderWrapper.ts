bd15d32e3976389ff782ee6f5ebb60af73acc627
emmanuel4365 <omalearumona@gmail.com>
2026-06-18 07:08:47 +0100

    added flex to logoNavbar and position fixed to header

--- frontend/src/components/wrappers/HeaderWrapper.ts
+++ frontend/src/components/wrappers/HeaderWrapper.ts
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
-const HeaderWrapper = styled.header``;
+const HeaderWrapper = styled.header`
+  position: sticky;
+  top: 0;
+`;
 
 export default HeaderWrapper;
--- frontend/src/components/wrappers/LogoNavbarWrapper.ts
+++ frontend/src/components/wrappers/LogoNavbarWrapper.ts
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
-const LogoNavbarWrapper = styled.div``;
+const LogoNavbarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: var(--pd-vertical-sect-align);
+
+  @media (min-width: 350px) {
+    .navbar {
+      display: none;
+    }
+    .consultation-btn {
+      display: none;
+    }
+  }
+`;
 
 export default LogoNavbarWrapper;
