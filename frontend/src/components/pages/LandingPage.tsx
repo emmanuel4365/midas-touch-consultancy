cee91fc8c5a85978ef29e3651f25cae4265cc3a1
emmanuel4365 <omalearumona@gmail.com>
2026-06-18 11:33:21 +0100

    initial completion of customer care section

--- frontend/src/components/pages/LandingPage.tsx
+++ frontend/src/components/pages/LandingPage.tsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router-dom";
-import { HeroSectionWrapper, LandingPageWrapper } from "../index";
+import { HeroSectionWrapper, LandingPageWrapper, CustomerCareWrapper } from "../index";
 import heroImage from "../../assets/hero-image.jpg";
+import cSupportIcon from "../../assets/customer-support-icon.svg";
+import cAdviceIcon from "../../assets/customer-advice-icon.svg";
+import cSatisfactionIcon from "../../assets/customer-satisfaction-icon.svg";
 
 
 const LandingPage = () => {
@@ -19,6 +22,35 @@ const LandingPage = () => {
                     <img src={heroImage} alt="Hero Image" className="hero-image" />
                 </div>
             </HeroSectionWrapper>
+            <CustomerCareWrapper>
+                <h2 className="customer-care-title">Customer Care Services</h2>
+                <div className="c-support-container">
+                    <img src={cSupportIcon} alt="Customer support icon" className="c-support-icon" />
+                    <div className="c-support-text-container">
+                        <h3>24/7 Support</h3>
+                        <p>Our team is available around the clock,
+                            providing assistance whenever
+                            you need it.
+                        </p>
+                    </div>
+                </div>
+                <div className="c-advice-container">
+                    <img src={cAdviceIcon} alt="Customer advice icon" className="c-advice-icon" />
+                    <div className="c-advice-text-container">
+                        <h3>Unlimited Advice</h3>
+                        <p>Get unlimited expert guidance across governance, finance, audit, and compliance.
+                        </p>
+                    </div>
+                </div>
+                <div className="c-satisfaction-container">
+                    <img src={cSatisfactionIcon} alt="Customer satisfaction icon" className="c-satisfaction-icon" />
+                    <div className="c-satisfaction-text-container">
+                        <h3>100% Customer satisfaction</h3>
+                        <p>We are committed to delivering exceptional results, and ensuring every client is fully satisfied.
+                        </p>
+                    </div>
+                </div>
+            </CustomerCareWrapper>
         </LandingPageWrapper>
     );
 };
--- frontend/src/components/wrappers/CustomerCareWrapper.ts
+++ frontend/src/components/wrappers/CustomerCareWrapper.ts
@@ -1,4 +1,44 @@
 import styled from "styled-components";
 
-const CustomerCareWrapper = styled.section``;
+const CustomerCareWrapper = styled.section`
+  @media (min-width: 350px) {
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--gap-24);
+    align-items: center;
+    justify-content: center;
+    padding: var(--pd-cust-care);
+  }
+
+  .customer-care-title {
+    display: none;
+  }
+
+  .c-support-container,
+  .c-advice-container,
+  .c-satisfaction-container {
+    display: flex;
+    align-items: center;
+    gap: var(--gap-2);
+    /* margin-bottom: 30px; */
+  }
+
+  .c-support-container p,
+  .c-advice-container p,
+  .c-satisfaction-container p {
+    font-size: var(--fs-tagline);
+  }
+
+  .c-support-container h3,
+  .c-advice-container h3,
+  .c-satisfaction-container h3 {
+    font-size: var(--fs-caption);
+  }
+
+  .c-support-text-container,
+  .c-advice-text-container,
+  .c-satisfaction-text-container {
+    width: 250px;
+  }
+`;
 export default CustomerCareWrapper;
